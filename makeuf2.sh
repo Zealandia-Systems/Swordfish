1a040de34821870c130e9703907dc124fd1e6d39
Scott Mohekey <smohekey@insightful.co.nz>
2022-03-16 13:48:34 +1300

    Enable creation of uf2 files.

--- makeuf2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for CONFIG in "Release"; do
-	for MACHINE in "P2" "P3" "P4"; do
-		./uf2conv.py -c -b 0x4000 -o "Swordfish-${CONFIG}-${MACHINE}.uf2" "${CONFIG} ${MACHINE}/Marlin.bin"
-	done
-done
