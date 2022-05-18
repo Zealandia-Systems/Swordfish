5e773fec40a57f269ef0ce72dcd65b2d7e3824dd
Scott Mohekey <smohekey@insightful.co.nz>
2022-05-18 14:19:30 +1200

    Fixes #51. (#52)
    * Fixes #51.
    
    * Call run_probe correctly from ToolsModule.cpp

--- src/marlin/gcode/probe/G38.cpp
+++ src/marlin/gcode/probe/G38.cpp
@@ -24,12 +24,12 @@
 
 #if ENABLED(G38_PROBE_TARGET)
 
-#include "../gcode.h"
+#	include "../gcode.h"
 
-#include "../../module/motion.h"
-#include "../../module/endstops.h"
+#	include "../../module/motion.h"
+#	include "../../module/endstops.h"
 
-extern bool run_probe(AxisEnum axis, EndstopEnum endstop, float distance);
+extern bool run_probe(AxisEnum axis, EndstopEnum endstop, float distance, float retract);
 
 /**
  * G38 Probe Target
@@ -45,39 +45,41 @@ extern bool run_probe(AxisEnum axis, EndstopEnum endstop, float distance);
 void GcodeSuite::G38(const int8_t subcode) {
 	KEEPALIVE_STATE(PROBING);
 
-  remember_feedrate_scaling_off();
+	remember_feedrate_scaling_off();
 
-  [[maybe_unused]] const bool error_on_fail =
-    #if ENABLED(G38_PROBE_AWAY)
-      !TEST(subcode, 0)
-    #else
-      (subcode == 2)
-    #endif
-  ;
-  
+	[[maybe_unused]] const bool error_on_fail =
+#	if ENABLED(G38_PROBE_AWAY)
+			!TEST(subcode, 0)
+#	else
+			(subcode == 2)
+#	endif
+			;
+
 	// If any axis has enough movement, do the move
-  LOOP_XYZ(i) {
-		if(parser.seenval(XYZ_CHAR(i))) {
-			const float v = parser.value_axis_units((AxisEnum)i);
-			const float dest = axis_is_relative((AxisEnum)i) ? current_position[i] + v : toNative(v, (AxisEnum)i);
-			
+	LOOP_XYZ(i) {
+		if (parser.seenval(XYZ_CHAR(i))) {
+			const float v = parser.value_axis_units((AxisEnum) i);
+			const float dest = axis_is_relative((AxisEnum) i) ? current_position[i] + v : toNative(v, (AxisEnum) i);
+
 			if (ABS(dest - current_position[i]) >= G38_MINIMUM_MOVE) {
-				//destination[i] = dest;
-				
-				endstops.enable_work_probe((AxisEnum)i, true);
-			
-				feedrate_mm_s = parser.seenval('F') ? parser.value_feedrate() : (homing_feedrate((AxisEnum)i) * 0.5);
-			
-				if(!run_probe((AxisEnum)i, WORK_PROBE, v)) SERIAL_ERROR_MSG("Failed to reach target");
-			
-				endstops.enable_work_probe((AxisEnum)i, false);
-				
+				// destination[i] = dest;
+
+				endstops.enable_work_probe((AxisEnum) i, true);
+
+				feedrate_mm_s = parser.seenval('F') ? parser.value_feedrate() : (homing_feedrate((AxisEnum) i) * 0.5);
+				float retract = parser.seenval('R') ? parser.value_float() : 5;
+
+				if (!run_probe((AxisEnum) i, WORK_PROBE, v, retract))
+					SERIAL_ERROR_MSG("Failed to reach target");
+
+				endstops.enable_work_probe((AxisEnum) i, false);
+
 				break;
 			}
 		}
 	}
 
-  restore_feedrate_and_scaling();
+	restore_feedrate_and_scaling();
 }
 
 #endif // G38_PROBE_TARGET
