1c28aece5c4b3b717c783415467fcef095a9693d
Scott Mohekey <smohekey@insightful.co.nz>
2022-08-18 13:57:27 +1200

    Interpret control state correctly for DZB312 and CT100 VFDs

--- src/swordfish/modules/tools/drivers/FulingDZBDriverImpl.cpp
+++ src/swordfish/modules/tools/drivers/FulingDZBDriverImpl.cpp
@@ -3,7 +3,7 @@
  *
  * Created: 14/10/2021 12:43:48 pm
  *  Author: smohekey
- */ 
+ */
 
 #include <swordfish/modules/estop/EStopModule.h>
 
@@ -13,132 +13,148 @@
 
 namespace swordfish::tools::drivers {
 	using namespace swordfish::estop;
-	
+
 	FulingDZBDriverImpl::ControlCommand FulingDZBDriverImpl::readControlCommand() const {
 		debug()();
-		
+
 		return readHoldingRegister(0x1000, ControlCommand::Unknown);
 	}
-	
+
 	void FulingDZBDriverImpl::writeControlCommand(ControlCommand controlCommand) const {
 		debug()();
-		
+
 		writeHoldingRegister(0x1000, controlCommand);
 	}
-	
+
 	FulingDZBDriverImpl::ControlState FulingDZBDriverImpl::readControlState() const {
 		debug()();
-		
+
 		return readHoldingRegister(0x1001, ControlState::Unknown);
 	}
-	
+
 	uint16_t FulingDZBDriverImpl::readFrequencyPercentage() const {
 		debug()();
-		
-		return readHoldingRegister(0x2000, (uint16_t)0);
+
+		return readHoldingRegister(0x2000, (uint16_t) 0);
 	}
 
 	uint16_t FulingDZBDriverImpl::readMaximumFrequency() const {
 		debug()();
-		
-		return readHoldingRegister(4, (uint16_t)0);
+
+		return readHoldingRegister(4, (uint16_t) 0);
 	}
-	
+
 	uint16_t FulingDZBDriverImpl::readFrequencyUpperLimit() const {
 		debug()();
-		
-		return readHoldingRegister(5, (uint16_t)0);
+
+		return readHoldingRegister(5, (uint16_t) 0);
 	}
 
 	uint16_t FulingDZBDriverImpl::readFrequencyLowerLimit() const {
 		debug()();
-		
-		return readHoldingRegister(6, (uint16_t)0);
+
+		return readHoldingRegister(6, (uint16_t) 0);
 	}
 
 	uint16_t FulingDZBDriverImpl::readOutputFrequency() const {
 		debug()();
-		
-		return readHoldingRegister(0x3001, (uint16_t)0);
+
+		return readHoldingRegister(0x3001, (uint16_t) 0);
 	}
-	
+
 	uint16_t FulingDZBDriverImpl::readOutputVoltage() const {
 		debug()();
-		
+
 		return readHoldingRegister(0x3003, 0);
 	}
-	
+
 	uint16_t FulingDZBDriverImpl::readOutputCurrent() const {
 		debug()();
-		
+
 		return readHoldingRegister(0x3002, 0);
 	}
-	
+
 	uint16_t FulingDZBDriverImpl::readDCBusVoltage() const {
 		debug()();
-		
+
 		return readHoldingRegister(0x3007, 0);
 	}
-	
+
 	State FulingDZBDriverImpl::readState() const {
 		debug()();
-		
-		auto controlState = (uint8_t)readControlState();
-		
-		return (State)(
-				((controlState & (uint8_t)ControlState::ForwardRunning || controlState & (uint8_t)ControlState::ReverseRunning) ? (uint8_t)State::Running : 0)
-			| (controlState & (uint8_t)ControlState::ForwardRunning ? (uint8_t)State::Forward : 0)
-			| (controlState & (uint8_t)ControlState::Fault ? (uint8_t)State::Fault : 0)
-		);
+
+		auto controlState = readControlState();
+
+		switch (controlState) {
+			case ControlState::ForwardRunning: {
+				return (State) ((uint8_t) State::Running | (uint8_t) State::Forward);
+			}
+
+			case ControlState::ReverseRunning: {
+				return State::Running;
+			}
+
+			case ControlState::Standby: {
+				return State::None;
+			}
+
+			case ControlState::Fault: {
+				return State::Fault;
+			}
+
+			default: {
+				return State::Fault;
+			}
+		}
 	}
-	
+
 	uint16_t FulingDZBDriverImpl::readFault() const {
 		debug()();
-		
+
 		auto fault = readHoldingRegister(0x5000, 0);
-		
+
 		debug()("fault: ", fault);
-		
-		if(fault == 17) {
+
+		if (fault == 17) {
 			auto& estopModule = EStopModule::getInstance();
-			
+
 			debug()("External fault detected.");
-			
-			if(!estopModule.isTriggered()) {
+
+			if (!estopModule.isTriggered()) {
 				writeHoldingRegister(0x1000, 0x0007);
 			}
 		}
-		
+
 		return fault;
 	}
-	
+
 	void FulingDZBDriverImpl::writeTargetFrequency(uint16_t targetFrequency) const {
 		debug()("targetFrequency: ", targetFrequency);
-			
+
 		uint16_t maximumFrequency = readMaximumFrequency();
-			
-		if(maximumFrequency == 0) {
+
+		if (maximumFrequency == 0) {
 			debug()("maximumFrequency can't be zero.");
-			
+
 			return;
 		}
-			
-		uint16_t targetFrequencyPercentage = ((float32_t)targetFrequency) / (float32_t)maximumFrequency * 10000.0f;
-			
+
+		uint16_t targetFrequencyPercentage = ((float32_t) targetFrequency) / (float32_t) maximumFrequency * 10000.0f;
+
 		debug()("targetFrequencyPercentage: ", targetFrequencyPercentage);
-			
+
 		writeHoldingRegister(0x2000, targetFrequencyPercentage);
 	}
-		
+
 	void FulingDZBDriverImpl::writeStart(Direction targetDirection) const {
-		debug()("targetDirection: ", (uint16_t)targetDirection);
-			
+		debug()("targetDirection: ", (uint16_t) targetDirection);
+
 		writeHoldingRegister(0x1000, targetDirection == Direction::Forward ? 0x0001 : 0x0002);
 	}
 
 	void FulingDZBDriverImpl::writeStop() const {
 		debug()();
-			
+
 		writeHoldingRegister(0x1000, 0x0005);
 	}
-}
+} // namespace swordfish::tools::drivers
